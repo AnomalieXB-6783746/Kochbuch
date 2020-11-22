1f0333a76b48f28e3dd5cb9762915e10ca1ba701
AnomalieXB-6783746 <56471811+AnomalieXB-6783746@users.noreply.github.com>
2020-11-22 14:03:17 +0100

    Kommunikation nur noch über https für mehr Sicherheit bezüglich Kennwörter etc

--- src/main/java/de/vilaca/Kochbuch/KochbuchApplication.java
+++ src/main/java/de/vilaca/Kochbuch/KochbuchApplication.java
@@ -1,16 +1,57 @@
 package de.vilaca.Kochbuch;
 
 import de.vilaca.Kochbuch.repositories.UserRepository;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = UserRepository.class)
 public class KochbuchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KochbuchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KochbuchApplication.class, args);
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+
+        // Add HTTP to HTTPS redirect
+        tomcat.addAdditionalTomcatConnectors(httpToHttpsRedirectConnector());
+
+        return tomcat;
+    }
+
+    /**
+     * As we need to redirect http queries to https for using ssl redirect
+     * all queries from port 8080 to https port 8443.
+     * @return a connector.
+     */
+    private Connector httpToHttpsRedirectConnector() {
+        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setScheme("http");
+        connector.setPort(8080);// The port the application would run on without SSL / https
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
 
 }
